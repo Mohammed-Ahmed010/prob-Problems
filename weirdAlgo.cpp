9fabf6296c711b9f01bd135055043ac89313596a
Mohammed-Ahmed010 <mohammedahme1212@gmail.com>
2024-02-11 16:30:43 +0530

    correct

--- weirdAlgo.cpp
+++ weirdAlgo.cpp
@@ -2,9 +2,9 @@
 using namespace std; 
 
 int main(){
-    int n;
+    long long n;
     cin>>n;
-    while(n!=1){
+    while(n>1){
         cout<<n<<' ';
         if(n&1) n=(n*3)+1;
         else n=n/2;
